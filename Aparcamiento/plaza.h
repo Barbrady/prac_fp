ede0cd2f8bb19cb9388f112802619cfa8c8bb53e
Barbrady <oficial.barbrady@gmail.com>
2014-12-16 18:06:37 +0100

    Actualización

---
+++ Aparcamiento/plaza.h
@@ -0,0 +1,32 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  plaza.h
+ *
+ *    Description:  Interfaz del TAD plaza
+ *
+ *        Version:  1.0
+ *        Created:  16/12/14 18:02:22
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#pragma once
+
+typedef enum TipoEstado  {
+    libre, ocupada};
+typedef enum TipoTamanio {
+    grande, pequeña};
+
+typedef struct TipoPlazaGaraje  {
+    int numero;
+    TipoEstado estado;
+    TipoTamanio tamanio;
+    void OcuparPlaza();
+    void LiberarPaza();
+    bool EstadoPlaza();
+}
---
+++ Aparcamiento/principal.cpp
@@ -0,0 +1,24 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  principal.cpp
+ *
+ *    Description:  Programa que controla un aparcamiento de 25 plazas
+ *
+ *        Version:  1.0
+ *        Created:  16/12/14 18:01:28
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+
+int main()  {
+
+}
+
