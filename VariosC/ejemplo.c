a45404c224f47f62e57ba43851266a4cafcdc2d3
Barbrady <barbrady@ASUS-PORTATIL>
2014-12-26 23:20:00 +0100

    Adjuntos

---
+++ VariosC/ejemplo.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int main( int argc, char *argv[])  {
+	int fd;
+
+	if( (fd = open( argv[1], O_RDWR )) ==-1 )
+	{
+		perror( "open" );
+		exit(-1);
+	}
+	printf("El fichero abierto tiene el descriptor %d.\n",fd);
+	close(fd);
+	return 0;
+}
