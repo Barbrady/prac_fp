786255b40a7528637988f8886805befe476ade1e
Oficial Barbrady <oficial.barbrady@gmail.com>
2015-11-03 12:23:53 +0100

    Actualización.

--- VariosC/ejemplo.c
+++ VariosC/ejemplo.c
@@ -2,10 +2,11 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <unistd.h>
 
 int main( int argc, char *argv[])  {
 	int fd;
-
+	printf("Número de argumentos = %d\n",argc);
 	if( (fd = open( argv[1], O_RDWR )) ==-1 )
 	{
 		perror( "open" );
